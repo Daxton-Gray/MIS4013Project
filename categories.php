5e77c0a09508dd954a47970117533e5f7d5cf847
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 14:38:08 -0600

    Update categories.php

--- categories.php
+++ categories.php
@@ -4,11 +4,9 @@ require_once("model/categories.php");
 
 $pageTitle = "Categories";
 include "view/header.php";
-?>
-      
-      <h1>Item Categories</h1>
-      
-<?php
+
+
+
 $categories = selectCategories();
 include "view/categories.php";
 include "view/footer.php";
