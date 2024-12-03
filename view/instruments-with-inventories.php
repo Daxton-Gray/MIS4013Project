f0986cc0f76995dc4aab2df19988ded5bf9d489c
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 17:51:15 -0600

    Create instruments-with-inventories.php

---
+++ view/instruments-with-inventories.php
@@ -0,0 +1,28 @@
+<h1>Instruments with Inventories</h1>
+<div class="card-group">
+<?php
+while ($instrument = $instruments->fetch_assoc()) {
+?>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?php echo $instrument['instrument_name']; ?></h5>
+      <p class="card-text">
+      <ul class="list-group">
+<?php
+  $inventories = selectInventoriesByInstrument($instrument['instrument_id']);
+  while ($inventory = $inventories->fetch_assoc()) {
+?>
+    <li class="list-group-item"><?php echo $inventory['brand_name']; ?> <?php echo $inventory['instrument name']; ?> - <?php echo $inventory['model']; ?> - <?php echo $inventory['price']; ?> Seasons - <?php echo $inventory['stock_quantity']; ?> MVPs</li>
+<?php
+  }
+?>
+      </ul>
+      </p>
+      
+    </div>
+  </div>
+
+<?php
+}
+?>
+</div>
