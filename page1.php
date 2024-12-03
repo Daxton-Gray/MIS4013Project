d4b826fecb18f99aea28e9b5ebcb930fea728f8b
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 14:30:02 -0600

    Update page1.php

--- page1.php
+++ page1.php
@@ -1,4 +1,7 @@
 <?php
+require_once("util-db.php");
+require_once("model/categories.php");
+
 $pageTitle = "Page 1";
 include "view/header.php";
 ?>
@@ -6,5 +9,7 @@ include "view/header.php";
       <h1>Page 1</h1>
       
 <?php
+$categories = selectCategories();
+include "view/categories.php";
 include "view/footer.php";
 ?>
