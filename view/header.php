181cf464a8b5fcb6d8b241e4301c3572dcdf2ca7
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 17:03:09 -0600

    Update header.php

--- view/header.php
+++ view/header.php
@@ -38,7 +38,7 @@
               </li> 
               
               <li class="nav-item">
-                <a class="nav-link" href="inventories.php">Inventories</a>
+                <a class="nav-link" href="inventories.php">Inventory</a>
               </li>
               
             </ul>
