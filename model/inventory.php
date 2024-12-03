fd3f82425900b8abb94ba366a5ea36cd84fecc1c
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 16:03:03 -0600

    Update inventory.php

--- model/inventory.php
+++ model/inventory.php
@@ -4,7 +4,7 @@
     try
       {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT * FROM inventory IN JOIN item IT ON IN.item_id=IT.item_id ORDER BY item_name, model");
+        $stmt = $conn->prepare("SELECT * FROM inventory IV JOIN item IT ON IV.item_id=IT.item_id ORDER BY item_name, model");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
