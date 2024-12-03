fe41b57307b668089e99b732d2f1190e79d99123
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 16:47:48 -0600

    Update instruments.php

--- model/instruments.php
+++ model/instruments.php
@@ -4,7 +4,7 @@
     try
       {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT * FROM instrument ORDER BY instrument_name");
+        $stmt = $conn->prepare("SELECT * FROM instrument I JOIN category C ON I.category_id=C.category_id ORDER BY instrument_name");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
