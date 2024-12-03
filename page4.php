e044b02bc019feed246df6ab296e9437d5236a25
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 14:08:58 -0600

    Update page4.php

--- page4.php
+++ page4.php
@@ -1,10 +1,10 @@
 <?php
 $pageTitle = "Page 4";
-include "view-header.php";
+include "view/header.php";
 ?>
       
       <h1>Page 4</h1>
       
 <?php
-include "view-footer.php";
+include "view/footer.php";
 ?>
