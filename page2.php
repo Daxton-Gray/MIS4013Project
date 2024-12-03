8c1c14dd4921b725578574ae693e5fac382ee18d
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 14:08:33 -0600

    Update page2.php

--- page2.php
+++ page2.php
@@ -1,10 +1,10 @@
 <?php
 $pageTitle = "Page 2";
-include "view-header.php";
+include "view/header.php";
 ?>
       
       <h1>Page 2</h1>
       
 <?php
-include "view-footer.php";
+include "view/footer.php";
 ?>
