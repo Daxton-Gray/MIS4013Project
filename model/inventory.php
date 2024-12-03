12fc045e89dc48c2dc4884e76d9e0b5537c82fb0
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 16:01:56 -0600

    Create inventory.php

---
+++ model/inventory.php
@@ -0,0 +1,19 @@
+<?php
+  function selectInventories() 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT * FROM inventory IN JOIN item IT ON IN.item_id=IT.item_id ORDER BY item_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+?>
