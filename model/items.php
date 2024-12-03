51d8a6d21dc49c616cfb81efcae57f7068ef4467
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 15:46:40 -0600

    Update items.php

--- model/items.php
+++ model/items.php
@@ -4,7 +4,7 @@
     try
       {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT * FROM item ORDER BY item_name, model");
+        $stmt = $conn->prepare("SELECT * FROM item I JOIN brand B ON I.brand_id=B.brand_id JOIN category C ON I.category_id=C.category_id ORDER BY item_name, model");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
