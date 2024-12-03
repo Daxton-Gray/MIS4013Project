d50b326269debc931ad7bbb974f421b2d6df6b7e
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 17:44:02 -0600

    Create instruments-with-inventories.php

---
+++ model/instruments-with-inventories.php
@@ -0,0 +1,37 @@
+<?php
+  function selectInstruments() 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT * FROM instrument I JOIN category C ON I.category_id=C.category_id ORDER BY instrument_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+
+  function selectInventoriesByInstruments() 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT * FROM inventory IV JOIN Instrument I ON I.instrument_id=IV.instrument_id ORDER BY instrument_name, model");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+?>
