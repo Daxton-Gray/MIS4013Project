604d6a2b6a151bf26db7414935545da4f19a4a62
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-04 10:31:39 -0600

    Update instruments-by-category.php

--- instruments-by-category.php
+++ instruments-by-category.php
@@ -5,7 +5,7 @@ require_once("model/instruments-by-category.php");
 $pageTitle = "Instruments by Category";
 include "view/header.php";
 
-$instruments = selectInstrumentsByCategory($_GET['id']);
+$instruments = selectInstrumentsByCategory($_GET['cid']);
 include "view/instruments-by-category.php";
 include "view/footer.php";
 ?>
