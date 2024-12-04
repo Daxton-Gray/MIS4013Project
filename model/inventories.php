900b68fb7346f1020f2d0031254e6a0552cc50ae
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 22:43:11 -0600

    Update inventories.php

--- model/inventories.php
+++ model/inventories.php
@@ -16,4 +16,94 @@
         throw $e;
       }
   }
+
+  function selectBrandssForInput() 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT brand_id, brand_name FROM brand ORDER BY brand_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+
+  function selectInstrumentsForInput() 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT instrument_id, instrument_name FROM instrument ORDER BY instrument_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+
+function insertInventory($brand_id, $instrument_id, $model, $price, $stock_quantity) 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `project_schema`.`inventory` (`inventory_id`, `brand_id`, `instrument_id`, `model`, `price`, `stock_quantity`) VALUES (NULL, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("iisfi", $brand_id, $instrument_id, $model, $price, $stock_quantity);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+
+function updateInventory($brand_id, $instrument_id, $model, $price, $stock_quantity, $inv_id) 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `project_schema`.`inventory` SET `brand_id` = ?, `instrument_id` = ?, `model` = ?, `price` = ?, `stock_quantity` = ? WHERE `inventory_id` = ?");
+        $stmt->bind_param("iisfii", $brand_id, $instrument_id, $model, $price, $stock_quantity, $inv_id);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+
+function deleteInventory($inv_id) 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `project_schema`.`inventory` WHERE instrument_id = ?");
+        $stmt->bind_param("i", $inv_id);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
 ?>
