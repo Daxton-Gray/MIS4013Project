b32732200c3bc5fa5af1e694061a78283bf492c2
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 18:26:49 -0600

    Update instruments-with-inventories.php

--- model/instruments-with-inventories.php
+++ model/instruments-with-inventories.php
@@ -22,7 +22,7 @@
     try
       {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT * FROM inventory IV JOIN Instrument I ON I.instrument_id=IV.instrument_id JOIN brand B ON B.brand_id=IV.brand_id");
+        $stmt = $conn->prepare("SELECT * FROM inventory IV JOIN Instrument I ON I.instrument_id=IV.instrument_id JOIN brand B ON B.brand_id=IV.brand_id WHERE IV.instrument=?");
         $stmt->bind_param("i", $inst_id);
         $stmt->execute();
         $result = $stmt->get_result();
