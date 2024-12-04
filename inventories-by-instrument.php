b45335bd3e1366140ce78cce6759d7d3a8350bad
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-04 10:05:14 -0600

    Update inventories-by-instrument.php

--- inventories-by-instrument.php
+++ inventories-by-instrument.php
@@ -5,7 +5,7 @@ require_once("model/inventories-by-instrument.php");
 $pageTitle = "Inventory by Instrument";
 include "view/header.php";
 
-$inventories = selectInventoriesByInstrument($_GET['id']);
+$inventories = selectInventoriesByInstrument($_GET['instrument_id']);
 include "view/inventories-by-instrument.php";
 include "view/footer.php";
 ?>
