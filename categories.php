bb18784443e1ddc6d03cc29e3a0700e60b864a53
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 19:25:17 -0600

    Update categories.php

--- categories.php
+++ categories.php
@@ -5,7 +5,31 @@ require_once("model/categories.php");
 $pageTitle = "Categories";
 include "view/header.php";
 
-
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertCategory($_POST['category_name'], $_POST['description'])) {
+        echo '<div class="alert alert-success" role="alert">Category Added</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+    case "Edit":
+      if (updateCategory($_POST['category_name'], $_POST['description'], $_POST['cid'])) {
+        echo '<div class="alert alert-success" role="alert">Category Edited</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteCategory($_POST['cid'])) {
+        echo '<div class="alert alert-success" role="alert">Category Deleted</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+  }
+}
 
 $categories = selectCategories();
 include "view/categories.php";
