6541bd303ebd61515784f1b8d4af70706c5a5105
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-04 11:27:53 -0600

    Create inventory-chart.php

---
+++ view/inventory-chart.php
@@ -0,0 +1,38 @@
+<h1>Inventory per Instrument</h1>
+
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+    datasets: [{
+        data: [
+<?php
+while ($inventory = $inventories->fetch_assoc()) {
+  echo $inventory['num_instruments'] . ", ";
+}
+?>          
+        ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+<?php
+$inventories = selectInventories();
+while ($inventory = $inventories->fetch_assoc()) {
+  echo "'" . $inventory['brand_name'] . " - " . $inventory['instrument_name'] . "', ";
+}
+?>  
+    ]
+},
+  });
+</script>
