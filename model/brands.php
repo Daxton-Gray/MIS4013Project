5cbbd13523c4a79a73ff8cf80f288a2f79583662
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 20:54:29 -0600

    Update brands.php

--- model/brands.php
+++ model/brands.php
@@ -16,4 +16,58 @@
         throw $e;
       }
   }
+
+function insertBrand($brand_name, $country_of_origin) 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `project_schema`.`brand` (`brand_id`, `brand_name`, `country_of_origin`) VALUES (NULL, ?, ?)");
+        $stmt->bind_param("ss", $brand_name, $country_of_origin);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+
+function updateBrand($brand_name, $country_of_origin, $bid) 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `project_schema`.`brand` SET brand_name = ?, country_of_origin = ? WHERE brand_id = ?");
+        $stmt->bind_param("ssi", $brand_name, $country_of_origin, $bid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
+
+function deleteBrand($bid) 
+  {
+    try
+      {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `project_schema`.`brand` WHERE brand_id = ?");
+        $stmt->bind_param("i", $bid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+      } 
+    catch (Exception $e) 
+      {
+        $conn->close();
+        throw $e;
+      }
+  }
 ?>
