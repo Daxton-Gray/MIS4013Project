f1103e2446f3a03725109497e79a9f0704119f5a
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 23:30:27 -0600

    Update instruments-with-inventories.php

--- view/instruments-with-inventories.php
+++ view/instruments-with-inventories.php
@@ -1,5 +1,11 @@
-<h1>Instruments with Inventories</h1>
-
+<div class="row">
+  <div class="col">
+    <h1>Instruments with Inventory</h1>
+  </div>
+  <div class="col-auto">
+<?php
+include "view/newform/inventories.php";
+?>
 <div class="card-group">
 <?php
 while ($instrument = $instruments->fetch_assoc()) {
@@ -13,7 +19,12 @@ while ($instrument = $instruments->fetch_assoc()) {
   $inventories = selectInventoriesByInstruments($instrument['instrument_id']);
   while ($inventory = $inventories->fetch_assoc()) {
 ?>
-    <li class="list-group-item"><?php echo $inventory['brand_name']; ?> <?php echo $inventory['instrument_name']; ?> - <?php echo $inventory['model']; ?> - $<?php echo $inventory['price']; ?> - <?php echo $inventory['stock_quantity']; ?> Units</li>
+    <li class="list-group-item">
+      <?php echo $inventory['brand_name']; ?> <?php echo $inventory['instrument_name']; ?>
+      <br/><?php echo $inventory['model']; ?>
+      <br/>$<?php echo $inventory['price']; ?>
+      <br/><?php echo $inventory['stock_quantity']; ?> Available
+    </li>
 <?php
   }
 ?>
