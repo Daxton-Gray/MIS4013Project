4dd1613c916c79c0fe1d6485fb04e74d0376bf9e
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-04 09:51:03 -0600

    Create inventories-by-instrument.php

---
+++ inventories-by-instrument.php
@@ -0,0 +1,11 @@
+<?php
+require_once("util-db.php");
+require_once("model/inventories-by-instrument.php");
+
+$pageTitle = "Inventory by Instrument";
+include "view/header.php";
+
+$inventories = selectInventoriesByInstrument($_GET['inst_id']);
+include "view/inventories-by-instrument.php";
+include "view/footer.php";
+?>
