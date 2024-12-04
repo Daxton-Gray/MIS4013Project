f0bb128398688620429cd11c8961243a5d0e911a
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 18:52:58 -0600

    Update header.php

--- view/header.php
+++ view/header.php
@@ -6,15 +6,6 @@
     <title><?=$pageTitle?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <style>
-      .scroll-container {
-        width: 100%;
-        overflow-x: auto;
-        whitespace: nowrap;
-        border: 1px solid #000;
-        padding: 10px;
-      }
-    </style>
   </head>
   
   <body>
