bae3a6ae03fd7b957e97230e2b9f86ae07e521f6
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 21:54:45 -0600

    Update categories.php

--- view/input-list/categories.php
+++ view/input-list/categories.php
@@ -1,1 +1,13 @@
-
+<select class="form-select" id="category_id" name="category_id">
+<?php
+while ($categoryItem = $categoryList->fetch_assoc()) {
+  $selText = "";
+  if ($selectedCategory == $categoryItem['category_id']) {
+    $selText = " selected";
+  }
+?>
+  <option value="<?php echo $categoryItem['category_id']; ?>"<?=$selText?>><?php echo $categoryItem['category_name']; ?></option>
+<?php
+}
+?>
+</select>
