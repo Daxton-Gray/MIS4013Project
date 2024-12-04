27b991b93c548874842d858cfaba123ad254d19c
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-04 10:23:29 -0600

    Create instruments-by-category.php

---
+++ view/instruments-by-category.php
@@ -0,0 +1,27 @@
+<h1>Instruments</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Instrument ID</th>
+        <th>Instrument Name</th>
+        <th>Category</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($instrument = $instruments->fetch_assoc()) {
+?>
+       <tr>
+         <td><?php echo $instrument['instrument_id']; ?></td>
+         <td><?php echo $instrument['instrument_name']; ?></td>
+         <td><?php echo $instrument['category_name']; ?></td>
+         <td><a href="inventories-by-instrument.php?id=<?php echo $instrument['instrument_id']; ?>">Inventory</a></td>
+       </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
