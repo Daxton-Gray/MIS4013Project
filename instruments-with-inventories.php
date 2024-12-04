5f4f3ec63bacca44314124f1125deb4bb4d688e2
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-03 23:24:18 -0600

    Update instruments-with-inventories.php

--- instruments-with-inventories.php
+++ instruments-with-inventories.php
@@ -5,7 +5,31 @@ require_once("model/instruments-with-inventories.php");
 $pageTitle = "Instruments with Inventories";
 include "view/header.php";
 
-
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertInventory($_POST['brand_id'], $_POST['instrument_id'], $_POST['model'], $_POST['price'], $_POST['stock_quantity'])) {
+        echo '<div class="alert alert-success" role="alert">Inventory Added</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+    case "Edit":
+      if (updateInventory($_POST['brand_id'], $_POST['instrument_id'], $_POST['model'], $_POST['price'], $_POST['stock_quantity'], $_POST['inv_id'])) {
+        echo '<div class="alert alert-success" role="alert">Inventory Edited</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteInventory($_POST['inv_id'])) {
+        echo '<div class="alert alert-success" role="alert">Inventory Deleted</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+  }
+}
 
 $instruments = selectInstruments();
 include "view/instruments-with-inventories.php";
