6f138d6d0b110e74cb0440e678446ea396eceff0
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-04 10:36:14 -0600

    Update instruments-with-inventories-by-brand.php

--- instruments-with-inventories-by-brand.php
+++ instruments-with-inventories-by-brand.php
@@ -5,7 +5,7 @@ require_once("model/instruments-with-inventories-by-brand.php");
 $pageTitle = "Instruments with Inventories by Brand";
 include "view/header.php";
 
-$instruments = selectInstrumentsByBrand($_GET['id']);
+$instruments = selectInstrumentsByBrand($_POST['cid']);
 include "view/instruments-with-inventories-by-brand.php";
 include "view/footer.php";
 ?>
