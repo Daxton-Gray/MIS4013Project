5b48d7e7abac271e58850558ddc21264fb201000
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-11 14:17:18 -0600

    Update header.php

--- view/header.php
+++ view/header.php
@@ -19,7 +19,7 @@
   
   <body>
     <div class="container">
-      <nav class="navbar navbar-expand-lg" style="background-color: transparent;">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
           
           <a class="navbar-brand" href="#">Navbar</a>
