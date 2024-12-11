cce4dbda2abb33eb4854fb31fabceae39d958fba
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-11 14:31:57 -0600

    Update header.php

--- view/header.php
+++ view/header.php
@@ -19,7 +19,7 @@
   
   <body>
     <div class="container">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary rounded">
+      <nav class="navbar navbar-expand-lg bg-light-subtle rounded">
         <div class="container-fluid">
           
           <a class="navbar-brand" href="#">Navbar</a>
