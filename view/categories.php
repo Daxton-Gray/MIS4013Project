ea30307c273bc9ee70f533214ef791b99894a390
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-11 21:34:27 -0600

    Update categories.php

--- view/categories.php
+++ view/categories.php
@@ -9,7 +9,7 @@ include "view/newform/categories.php";
   </div>
 </div>
 <div class="table-responsive rounded">
-  <table class="table">
+  <table class="table rounded">
     <thead>
       <tr>
         <th>Category ID</th>
