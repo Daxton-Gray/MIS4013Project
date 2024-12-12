c03335e54e5b55731fd5c06f1b19f3adcb0f02d8
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-12 11:25:15 -0600

    Update header.php

--- view/header.php
+++ view/header.php
@@ -13,14 +13,6 @@
         background-attachment: fixed;
         background-size: 100% 100%;
       }
-
-      .modal {
-        z-index: 1050;
-      }
-      .modal-backdrop {
-        z-index: 1040;
-      }
-
     </style>
 
   </head>
