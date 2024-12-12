7ec10199369f614a4bc6df21360fa25adcaa9251
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-12 00:08:21 -0600

    Update inventory-chart-db.php

--- model/inventory-chart-db.php
+++ model/inventory-chart-db.php
@@ -2,7 +2,7 @@
 function selectInventories() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT COUNT(IV.instrument_id) AS num_instruments, instrument_name, brand_name FROM instrument I JOIN Inventory IV ON I.instrument_id=IV.instrument_id JOIN brand B ON B.brand_id=IV.brand_id GROUP BY instrument_name, brand_name");
+        $stmt = $conn->prepare("SELECT COUNT(IV.instrument_id) AS num_instruments, instrument_name, brand_name FROM instrument I JOIN Inventory IV ON I.instrument_id=IV.instrument_id JOIN brand B ON B.brand_id=IV.brand_id GROUP BY instrument_name, brand_name ORDER BY instrument_name, brand_name");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
