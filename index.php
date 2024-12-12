3476af6f675184acc8c1165e034e71d04175b67a
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-12 13:31:13 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,27 @@
 $pageTitle = "Home";
 include "view/header.php";
 ?>
+
+function displayGreeting() {
+    const now = new Date();
+    const hour = now.getHours();
+    let greeting = "Hello!";
+    if (hour < 12) {
+        greeting = "Good Morning!";
+    } else if (hour < 18) {
+        greeting = "Good Afternoon!";
+    } else {
+        greeting = "Good Evening!";
+    }
+    document.getElementById('greeting').innerText = greeting;
+}
+
       
       <h1>DG Music Supply</h1>
+
+      <p id="greeting">Welcome!</p>
+      <script>displayGreeting();</script>
+
       <p style="color:#e5e4e2;"><strong>At DG Music Supply, we believe music is more than just sound – it's a universal language that connects us all. Whether you're a seasoned musician, an aspiring artist, or someone simply looking to explore the magic of music, we are here to support your journey.</strong></p>
       
 <?php
