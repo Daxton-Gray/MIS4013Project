b37348c9002dbe5a915a456d605c8b5ac5a09b30
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-12 15:43:22 -0600

    Update brands.php

--- view/editform/brands.php
+++ view/editform/brands.php
@@ -1,12 +1,12 @@
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editBrandModal<? echo $brand['brand_id'];?>">
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editBrandModal">
   Open Modal
 </button>
 
-<div class="modal fade" id="editBrandModal<? echo $brand['brand_id'];?>" tabindex="-1" aria-labelledby="editBrandModalLabel<? echo $brand['brand_id'];?>" aria-hidden="true">
+<div class="modal fade" id="editBrandModal" tabindex="-1" aria-labelledby="editBrandModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editBrandModalLabel1">Edit Brand</h1>
+        <h1 class="modal-title fs-5" id="editBrandModalLabel">Edit Brand</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
