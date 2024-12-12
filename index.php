4580f5b2c33234a34aa0df8f8c0a2460ed021032
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-12 01:15:30 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ include "view/header.php";
 ?>
       
       <h1>DG Music Supply</h1>
+      <p>At DG Music Supply, we believe music is more than just sound – it's a universal language that connects us all. Whether you're a seasoned musician, an aspiring artist, or someone simply looking to explore the magic of music, we are here to support your journey.</p>
       
 <?php
 include "view/footer.php";
